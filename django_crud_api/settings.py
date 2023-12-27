3dff8a48fd97518dfa69267af090a51160b475ac
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-27 15:20:13 -0300

    ADD docs

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'tasks'
+    'coreapi',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS=[]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
