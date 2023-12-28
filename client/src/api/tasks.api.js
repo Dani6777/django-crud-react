ce7b799f4e27993905e191e9563922e64abd80c2
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-28 00:39:54 -0300

    ADD update task

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -4,8 +4,12 @@ import axios from 'axios'
      baseURL:'http://localhost:8000/tasks/api/v1/tasks'
 });
 
-export const getAllTasks =() => tasksApi.get('/')
+export const getAllTasks = () => tasksApi.get('/')
+
+export const getTask = (id) => tasksApi.get(`/${id}/`)
 
 export const createTask = (task) => tasksApi.post('/', task)
 
 export  const deleteTask = (id) => tasksApi.delete(`/${id}`)
+
+export const updateTask = (id, task) => tasksApi.put(`/${id}/`,task)
