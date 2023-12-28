1f83c8059f527b1844a10472a77b886e8b6e64d8
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-27 23:26:02 -0300

    ADD create task

--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,6 +1,23 @@
-export function TasksFormPage(){
-    return(
-        <div>Task Form Page</div>
+import { createTask } from '../api/tasks.api'
+import {useForm} from 'react-hook-form'
+import {useNavigate} from 'react-router-dom'
+
+export function TasksFormPage() {
+
+    const {register, handleSubmit, formState:{errors}} = useForm();
+    const  navigate = useNavigate();
+    const onSubmit = handleSubmit( async data => { await createTask( data ); navigate('/tasks');});
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type='text' placeholder='title' {...register('title',{required:true})} />
+                {errors.title && <span>this field is required</span>}
+                <textarea rows='3' placeholder='description' {...register('description',{required:true})}></textarea>
+                {errors.description && <span>this field is required</span>}
+                <button>Save</button>
+            </form>
+        </div>
     )
 }
 
